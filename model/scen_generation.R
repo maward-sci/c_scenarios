3c6115ba8d02ad449119e7e720407d9d249de3ad
Connor <cddibble@gmail.com>
2022-08-27 16:44:21 -0700

    docstrings

--- model/scen_generation.R
+++ model/scen_generation.R
@@ -2,6 +2,7 @@
 # using reference model paramter values
 # from the model_params dataframe
 library(dplyr)
+library(docstring)
 ################################
 # Define Paramters
 ################################
@@ -47,7 +48,27 @@ model_params <- data.frame(
 # https://www.tjmahr.com/anatomy-of-a-logistic-growth-curve/
 # test: plot(logistic_growth(0:10))
 
-log_growth_general <- function(years, scale = 1, asymptote = 60, midpoint, year_midpoint){
+log_growth_general <- function(years, scale = 1, asymptote = 60, year_midpoint = NULL, midpoint = NULL ){
+  #' log_growth_general
+  #' 
+  #' @description Compute logistic growth curve as a function of integer "years"
+  #'
+  #' @param years list. a list of integers representing successive years in the experiment
+  #' @param scale integer. used to scale the result of (midpoint - years) in the logistic growth function
+  #' @param asymptote integer  the saturation (maximum) value of the response variable in the growth curve
+  #' @param year_midpoint integer  Specifies the year (i.e., x-axis value) that should represent the midpoint of the growth curve
+  #' Used to compute the `midpoint` parameter, unless `midpoint` is specified.
+  #' @param midpoint integer  Optionally take control of the midpoint parameter in the logistic growth equation
+  #' if used, ignores year_midpoint argument.
+  
+  #' @usage transplant = log_growth_general(
+  #'                        years = 1:10,
+  #'                        scale = 1,
+  #'                        asymptote = 60,
+  #'                        midpoint = NULL,
+  #'                        year_midpoint = 4.3
+  #'                      )
+  #' @return list of length `length(years)` - The logistic growth values of the response variable over time.
   asymptote = asymptote # max size of meadow
   scale=scale
   if (is.null(midpoint)){
@@ -59,10 +80,22 @@ log_growth_general <- function(years, scale = 1, asymptote = 60, midpoint, year_
 
 
 ### generic version 
-carbon_per_vegetated_area <- function(area, carbon_param){
-  # Scales methane emissions as a function of area in hectares for vegetated habitats
-  # Parameters
-  # area: list the areas in hectares to transform
+carbon_per_vegetated_area <- function(area, carbon_param, model_params){
+  #' carbon_per_vegetated_area
+  #' 
+  #' @description Compute the carbon constituent value as a function of area
+  #'
+  #' @param area list. a list of numeric values representing the area in hectares of a plot over time
+  #' @param carbon_param string. one of `methane`, `nitrous_oxide`, `biomass`, or `soil`
+  #' @param model_params Dataframe the table of parameters for vegetated and unvegetated areas for each carbon_param
+  
+  #' @usage carbon_per_vegetated_area(
+  #'  area = 1:10,
+  #'  carbon_param = 'methane',
+  #'  model_params = model_params
+  #'  )
+  #' @return list of length `length(area)` - The `carbon_param` values as a function of area given model_params
+  # Scales emissions as a function of area in hectares for vegetated habitats
   carbon_rest <- area * as.numeric(
     rnorm(
       n = length(area),
@@ -70,7 +103,6 @@ carbon_per_vegetated_area <- function(area, carbon_param){
       sd = as.numeric(model_params$sd_vegetated[carbon_param])
     )
   )
-  # print(meth_rest)
   return(carbon_rest)
 }
 
@@ -139,7 +171,8 @@ create_seagrass_exp <- function(model_params, n_sim){
   # set methane for Restoration where it is dependent on project_size_ha
   df[which(df$restoration_status=='Restoration'), 'methane'] <- carbon_per_vegetated_area(
     area = df[which(df$restoration_status=='Restoration'), 'project_size_ha'],
-    carbon_param = 'methane'
+    carbon_param = 'methane',
+    model_params = model_params
     )
   df$nitrous_oxide <- rnorm(
     n = nrow(df), #draw from the normal distribution nrow times
@@ -148,7 +181,8 @@ create_seagrass_exp <- function(model_params, n_sim){
     )
   df[which(df$restoration_status=='Restoration'), 'nitrous_oxide'] <- carbon_per_vegetated_area(
     df[which(df$restoration_status=='Restoration'), 'project_size_ha'],
-    carbon_param = 'nitrous_oxide'
+    carbon_param = 'nitrous_oxide',
+    model_params = model_params
   )
   df$biomass <- rnorm(
     n = nrow(df), #draw from the normal distribution nrow times
@@ -157,7 +191,8 @@ create_seagrass_exp <- function(model_params, n_sim){
   )
   df[which(df$restoration_status=='Restoration'), 'biomass'] <- carbon_per_vegetated_area(
     df[which(df$restoration_status=='Restoration'), 'project_size_ha'],
-    carbon_param = 'biomass'
+    carbon_param = 'biomass',
+    model_params = model_params
   )
   df$soil <- rnorm(
     n = nrow(df), #draw from the normal distribution nrow times
@@ -166,7 +201,8 @@ create_seagrass_exp <- function(model_params, n_sim){
   )
   df[which(df$restoration_status=='Restoration'), 'soil'] <- carbon_per_vegetated_area(
     df[which(df$restoration_status=='Restoration'), 'project_size_ha'],
-    carbon_param = 'soil'
+    carbon_param = 'soil',
+    model_params = model_params
   )
   # set N2O for Restoration where it is dependant on project_size_ha
   # Modify some parameters based on descriptive variables
@@ -183,3 +219,37 @@ summarize_simulations <- function(df){
     .funs = c(mean = mean, sd = sd), na.rm=TRUE)
   return(summary)
 }
+
+
+
+# DOCSTRINGS TEMPLATE
+  #' Paste two items
+  #' 
+  #' @description This function pastes two items
+  #' together.  
+  #'
+  #' By using the description tag you'll notice that I
+  #' can have multiple paragraphs in the description section
+  #' 
+  #' @param x character. The first item to paste
+  #' @param y character. The second item to paste Defaults to "!" but
+  #' "?" would be pretty great too
+  #' @usage mypaste(x, y)
+  #' @return The inputs pasted together as a character string.
+  #' @details The inputs can be anything that can be input into
+  #' the paste function.
+  #' @note And here is a note. Isn't it nice?
+  #' @section I Must Warn You:
+  #' The reference provided is a good read.
+  #' \subsection{Other warning}{
+  #'   It is completely irrelevant to this function though.
+  #' }
+  #' 
+  #' @references Tufte, E. R. (2001). The visual display of 
+  #' quantitative information. Cheshire, Conn: Graphics Press.
+  #' @examples
+  #' mypaste(1, 3)
+  #' mypaste("hey", "you")
+  #' mypaste("single param")
+  #' @export
+  #' @importFrom base paste
