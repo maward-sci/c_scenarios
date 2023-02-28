cf18ae40791d0817c5ca939dfaabbf445fcc814d
Melissa Ward <maward@ucdavis.edu>
2023-02-28 12:17:09 -0800

    veg to mgmt term changes
    changed terms from unveg to bau and veg to mgmt

--- scenarios/model_simulations.R
+++ scenarios/model_simulations.R
@@ -13,7 +13,7 @@ df <- create_seagrass_exp(
 
 df <- compute_totals(df)
 
-ggplot(df) + geom_line(aes(x = year, y = methane_net_gain, color = sim, group = sim))
+ggplot(df) + geom_line(aes(x = year, y = methane_netgain, color = sim, group = sim))
 
 ### Melt and summarize the simulation data
 mdf <- melt_simulation_df(df)
@@ -40,8 +40,6 @@ ggplot(sum_df) +
 ## seems like there are some issues with the soil BAU for conservation, need to look into this. 
 
 
-
-
 #### confused about what all this crap below is ###
 
 ##### plot melted summaries ####
@@ -49,59 +47,88 @@ ggplot(mdf_summary) +
   geom_line(aes(x = year, y = value_mean, color = scenario)) +
   facet_grid(treatment~metric)
 
-ggplot(mdf_summary, aes(x= year, y=value_mean, color = treatments)) +
+ggplot(mdf_summary, aes(x= year, y=value_mean, color = treatment)) +
   geom_point() + geom_smooth() + theme_bw() + 
-  facet_grid(restoration_status~metric)
+  facet_grid(treatment~metric)
 
 
 ###### Summarize raw data and plot plots #######
 summary <- summarize_simulations(df) #summarize_simulations in Scen_generation file
 
-## methane ##
-  #restoration scenario 
+#### METHANE ####
+#restoration scenario 
 ggplot(summary) +
-  geom_line(aes(x = year, y = methane_carbon_rest_mean)) +
-  geom_ribbon(aes(x = year, ymin = methane_carbon_rest_mean - methane_carbon_rest_sd, ymax = methane_carbon_rest_mean + methane_carbon_rest_sd), inherit.aes=TRUE)# +
-  #facet_grid(treatments~restoration_status)
+  geom_line(aes(x = year, y = methane_mgmt_mean)) +
+  geom_ribbon(aes(x = year, ymin = methane_mgmt_mean - methane_mgmt_sd, ymax = methane_mgmt_mean + methane_mgmt_sd), inherit.aes=TRUE)# +
+#facet_grid(scenario~treatment)
 
-ggplot(summary, aes(year, methane_carbon_rest_mean, color= treatments)) + 
-  geom_point() + geom_smooth() + theme_bw()
-
-  #baseline scenario
-ggplot(summary, aes(year, methane_carbon_unvegetated_mean, color= treatments)) + 
-  geom_point() + geom_smooth() + theme_bw()
-
-
-## N2O ##
-  #restoration/project scenarios 
-ggplot(summary) +
-  geom_line(aes(x = year, y = nox_carbon_rest_mean)) + 
-  geom_ribbon(aes(x = year, ymin = nox_carbon_rest_mean - nox_carbon_rest_sd, ymax = nox_carbon_rest_mean + nox_carbon_rest_sd), inherit.aes=TRUE)# +
-#facet_grid(treatments~restoration_status)
-
-ggplot(summary, aes(year, nox_carbon_rest_mean, color= treatments)) + 
-  geom_point() + geom_smooth() + theme_classic()
-#baseline scenario
-ggplot(summary, aes(year, nox_carbon_unvegetated_mean, color= treatments)) + 
-  geom_point() + geom_smooth() + theme_bw()
-
-
-## Biomass ##
-ggplot(summary) +
-  geom_line(aes(x = year, y = biomass_carbon_rest_mean)) +
-  geom_ribbon(aes(x = year, ymin = biomass_carbon_rest_mean - biomass_carbon_rest_sd, ymax = biomass_carbon_rest_mean + biomass_carbon_rest_sd), inherit.aes=TRUE)
-## Soil ##
-ggplot(summary) +
-  geom_line(aes(x = year, y = soil_carbon_rest_mean)) +
-  geom_ribbon(aes(x = year, ymin = soil_carbon_rest_mean - soil_carbon_rest_sd, ymax = soil_carbon_rest_mean + soil_carbon_rest_sd), inherit.aes=TRUE)# +
-#facet_grid(treatments~restoration_status)
-
-ggplot(summary, aes(year, soil_carbon_rest_mean, color= treatments)) + 
-  geom_point() + geom_smooth() + theme_bw()
+ggplot(summary, aes(year, methane_mgmt_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-10,60) + ylab('Methane MGMT (Ceq/m2)')
 
 #baseline scenario
-ggplot(summary, aes(year, soil_carbon_unvegetated_mean, color= treatments)) + 
-  geom_point() + geom_smooth() + theme_bw()
+ggplot(summary, aes(year, methane_bau_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-10,60) + ylab('Methane BAU (Ceq/m2)')
+
+#net gain  
+ggplot(summary, aes(year, methane_netgain_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-10,60) + ylab('Methane Net Gain (Ceq/m2)')
+
+
+
+#### NOX ####
+#restoration scenario 
+ggplot(summary) +
+  geom_line(aes(x = year, y = nox_mgmt_mean)) +
+  geom_ribbon(aes(x = year, ymin = nox_mgmt_mean - nox_mgmt_sd, ymax = nox_mgmt_mean + nox_mgmt_sd), inherit.aes=TRUE)# +
+#facet_grid(scenario~treatment)
+
+ggplot(summary, aes(year, nox_mgmt_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-10,40) + ylab('NOX MGMT (Ceq/m2)')
+
+#baseline scenario
+ggplot(summary, aes(year, nox_bau_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-10,40) + ylab('NOX BAU (Ceq/m2)')
+
+#net gain  
+ggplot(summary, aes(year, nox_netgain_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-10,40) + ylab('NOX Net Gain (Ceq/m2)')
+
+
+#### BIOMASS ####
+#restoration scenario 
+ggplot(summary, aes(year, biomass_mgmt_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-500,1200) + ylab('Biomass MGMT (Ceq/m2)') 
+
+#baseline scenario
+ggplot(summary, aes(year, biomass_bau_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-500,1200) + ylab('Biomass BAU (Ceq/m2)')
+
+#net gain  
+ggplot(summary, aes(year, biomass_netgain_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-1200,1200) + ylab('Biomass Net Gain (Ceq/m2)')
+
+
+
+#### SOIL ####
+#restoration scenario 
+ggplot(summary, aes(year, soil_mgmt_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylab('Soil MGMT (Ceq/m2)') 
+
+#baseline scenario
+ggplot(summary, aes(year, soil_bau_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylim(-50,300) + ylab('Soil BAU (Ceq/m2)') 
+
+#net gain  
+ggplot(summary, aes(year, biomass_netgain_mean, color= scenario)) + 
+  geom_point() + geom_smooth() + theme_bw() + ylab('Biomass Net Gain (Ceq/m2)')
+
+
+
+
+
+
+
+
 
 ## Net Carbon Gain ##
 ggplot(summary) +
